447d4d645aefac6e89774ae0211da1bf70405948
HPPavilion2005 <loveyaemiko2k5@gmail.com>
2026-05-10 14:18:39 +0700

    Update

--- QuanLyCamDo.sql
+++ QuanLyCamDo.sql
@@ -14,3 +14,70 @@ CREATE TABLE KhachHang (
     DiaChi NVARCHAR(255)
 
 );
+
+CREATE TABLE HopDong (
+
+    MaHD INT PRIMARY KEY IDENTITY(1,1),
+
+    MaKH INT,
+
+    NgayVay DATE,
+
+    TienGoc DECIMAL(18,2),
+
+    Deadline1 DATE,
+
+    Deadline2 DATE,
+
+    TrangThai NVARCHAR(50),
+
+    FOREIGN KEY (MaKH)
+        REFERENCES KhachHang(MaKH)
+
+);
+
+CREATE TABLE TaiSan (
+
+    MaTS INT PRIMARY KEY IDENTITY(1,1),
+
+    TenTaiSan NVARCHAR(100),
+
+    GiaTriDinhGia DECIMAL(18,2),
+
+    TrangThai NVARCHAR(50)
+
+);
+
+CREATE TABLE HopDong_TaiSan (
+
+    MaHD INT,
+
+    MaTS INT,
+
+    PRIMARY KEY (MaHD, MaTS),
+
+    FOREIGN KEY (MaHD)
+        REFERENCES HopDong(MaHD),
+
+    FOREIGN KEY (MaTS)
+        REFERENCES TaiSan(MaTS)
+
+);
+
+CREATE TABLE ThanhToan (
+
+    MaTT INT PRIMARY KEY IDENTITY(1,1),
+
+    MaHD INT,
+
+    NgayThanhToan DATE,
+
+    SoTienTra DECIMAL(18,2),
+
+    NguoiThu NVARCHAR(100),
+
+    FOREIGN KEY (MaHD)
+        REFERENCES HopDong(MaHD)
+
+);
+
